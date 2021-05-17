177879b17de9d0d0ab2be2e2de8514efe772124e
onlyabhilash <77659887+onlyabhilash@users.noreply.github.com>
2021-05-17 23:00:32 +0530

    ML

--- BangaloreHousePricePrediction/server/server.py
+++ BangaloreHousePricePrediction/server/server.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
-#import util
+import util
 
 app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'HI'
 
 if __name__ == '__main__':
     print("Starting Python Flask Server For Home Price Prediction...")
